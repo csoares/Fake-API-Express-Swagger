bb2c5da6a4a3440f6515881d8a8969f0d312167b
Christophe Soares <christophe.p.soares@mindera.com>
2023-04-27 14:27:11 +0100

    add hot reload

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger_output.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
 
-app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 // For legacy browser support
-}
+  origin: "*",
+  optionsSuccessStatus: 200, // For legacy browser support
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 const port = 5001;
-require('./endpoints')(app)
-
+require("./endpoints")(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
